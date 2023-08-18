6b47a62660299488da74473deb1fa2cd31f2b76e
Mohammad Ataei <mammadataei@gmail.com>
2023-08-18 17:15:20 +0330

    feat: add navigation menu

--- config/navbar.ts
+++ config/navbar.ts
@@ -1,23 +1,64 @@
-export type NavbarItem = {
+export type NavigationMenu = {
+  title: string
+  items: Array<NavigationItem>
+}
+
+export type NavigationItem = {
   title: string
   href: string
   disabled?: boolean
+  description?: string
 }
-export const navbarItems: Array<NavbarItem> = [
+
+export const navbarItems: Array<NavigationItem | NavigationMenu> = [
   {
-    title: 'Documentation',
-    href: 'https://docs.hyperdbg.org/',
+    title: 'Docs',
+    items: [
+      {
+        title: 'Documentation',
+        href: 'https://docs.hyperdbg.org',
+        description: 'Read the documentation to learn how to use HyperDbg.',
+      },
+      {
+        title: 'Doxygen',
+        href: 'https://doxygen.hyperdbg.org/',
+        description: 'Read the doxygen to learn how to use HyperDbg.',
+      },
+      {
+        title: 'Cheatsheet',
+        href: '/cheatsheet',
+        description: 'Learn how to use HyperDbg with examples.',
+      },
+    ],
   },
   {
-    title: 'Doxygen',
-    href: 'https://doxygen.hyperdbg.org/',
+    title: 'Learn',
+    items: [
+      {
+        title: 'Tutorials',
+        href: '/tutorials',
+        description: 'Learn how to use HyperDbg with examples.',
+      },
+    ],
   },
   {
-    title: 'Tutorials',
-    href: '/tutorials',
+    title: 'Community',
+    items: [
+      {
+        title: 'Issues',
+        href: '',
+        description: 'Report a bug or request a feature.',
+      },
+      {
+        title: 'Discussions',
+        href: '',
+        description: 'Ask questions and find answers.',
+      },
+    ],
   },
   {
     title: 'Blog',
     href: '/blog',
+    description: 'Read the latest news about HyperDbg.',
   },
 ]
