65a8ddde4d7e1adf94e0fe22675a9e8a90c4f0dc
Mohammad Ataei <80059702+mammadataei@users.noreply.github.com>
2023-08-18 18:38:01 +0330

    chore: update heading font (#8)

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,11 +1,12 @@
-import { Inter as FontSans, Acme as FontHeading } from 'next/font/google'
+import { Inter as FontSans, Rubik as FontHeading } from 'next/font/google'
 
 export const fontSans = FontSans({
   subsets: ['latin'],
   variable: '--font-sans',
 })
+
 export const fontHeading = FontHeading({
-  weight: ['400'],
+  weight: ['500'],
   subsets: ['latin'],
   variable: '--font-heading',
 })
