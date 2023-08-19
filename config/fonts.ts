98c330d16479f23d006598a8b9348b78e41030c4
Mohammad Ataei <mammadataei@gmail.com>
2023-08-19 18:52:30 +0330

    chore: update heading font

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,12 +1,12 @@
-import { Inter as FontSans, Rubik as FontHeading } from 'next/font/google'
+import { Inter as FontSans } from 'next/font/google'
+import localFont from 'next/font/local'
 
 export const fontSans = FontSans({
   subsets: ['latin'],
   variable: '--font-sans',
 })
 
-export const fontHeading = FontHeading({
-  weight: ['500'],
-  subsets: ['latin'],
+export const fontHeading = localFont({
+  src: '../assets/fonts/CalSans-SemiBold.woff2',
   variable: '--font-heading',
 })
